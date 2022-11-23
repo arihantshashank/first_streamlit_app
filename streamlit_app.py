439db8aa19e521a115115fd2d4a275e0b4a6b8a1
arihantshashank <118901507+arihantshashank@users.noreply.github.com>
2022-11-23 17:45:22 +0530

    Update streamlit_app.py
    added some texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast menu")
+streamlit.text("Omega 3 and blueberry oat meal")
+streamlit.text("idly and sambhar")
+streamlit.text("aloo paratha and curd")
